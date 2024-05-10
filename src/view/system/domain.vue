e13557d2b5daf92b121aa792be95e4bd15c0f335
moreyLee <764457208@qq.com>
2024-05-11 00:21:21 +0400

    新增短域名创建

--- src/view/system/domain.vue
+++ src/view/system/domain.vue
@@ -1,12 +1,11 @@
 <template>
-  <el-form ref="domain" v-model="domain" :rules="rules">
+  <div>
+  <el-form ref="domainForm" :model="domainInfo" :rules="rules">
   <el-form-item label="短域名">
-    <el-input v-model="short_url" style="width: 240px"
-              type="text"
+    <el-input v-model="domainInfo.name" style="width: 240px"
               placeholder="请输入短域名"
               clearable
               />
-
   </el-form-item>
 <!--  <el-form-item label="代理链接">-->
 <!--    <el-input v-model="agent_url" style="width: 240px"  placeholder="请输入代理邀请链接" />-->
@@ -15,30 +14,49 @@
     <el-button type="primary" @click="submitDomain" style="width: 5%; margin-top: 5px">新建域名</el-button>
   </el-form-item>
   </el-form>
+  </div>
 </template>
 <script setup lang="ts">
 import {ref, reactive} from 'vue'
 import {domain} from "@/api/test";
+import {ElMessage} from "element-plus";
 
-// v-model将输入框的值和short_url 双向绑定
-const short_url = ref("ss36.vip")
-const data = [
-  {
-    name: short_url.value,
-    jump_start: "true",
-
-  }
-]
-const agent_url = ref('')
+// 定义数据类型接口
+interface DomainInfo {
+  name: string;
+  jump_start : string
+}
+// 定义了一个domainInfo的响应式变量
+// 其类型为 DomainInfo 描述domainInfo 的数据结构
+const domainInfo = ref<DomainInfo>({
+  name: "",
+  jump_start : "true"
+})
+// 定义传入到后端的json body 中的数据  定义的数据
+const data ={
+  name: domainInfo.value.name,
+  jump_start: "true"
+}
 const rules = reactive({
-  short_url: [{ required: true, message: '请输入短域名', trigger: 'blur' , isValidHttpUrl: true}],
+  name: [{ required: true, message: '请输入短域名', trigger: 'blur' , isValidHttpUrl: true}],
 })
 const submitDomain = async () => {
-  const json = qs.stringify(this.domain);
-  console.log("短域名", short_url.value)
-  console.log("json数据", data)
-  const resp = await domain(json)
+
+  // console.log("短域名", domainInfo.value)
+  const resp = await domain(data)
   console.log("响应返回:", resp.data)
+  if (resp.data.result.success === true) {
+    ElMessage({
+      message: '创建域名成功',
+      type: 'success',
+    })
+  }
+  if (resp.data.result.success === false) {
+    ElMessage({
+      message: '域名创建失败或域名已存在',
+      type: 'error',
+    })
+  }
 }
 
 </script>
